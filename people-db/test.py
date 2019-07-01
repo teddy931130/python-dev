07f1c2955cf82e4531e8c95a164b0997e3346f2e
Teodor Daskalov <teo_das@abv.bg>
2019-07-01 13:37:59 +0300

    initial idea for people-db

---
+++ people-db/test.py
@@ -0,0 +1,29 @@
+
+def open_database():
+    contacts = {}
+    with open("database.txt", "r") as file:
+        # File structure: Georgi | name: Georgi, age: 25, city: Sofia
+        # remove \n from end of each line in the file
+        lines = file.read().splitlines()
+
+        # get left and right part for the entry
+        for line in lines:
+            key = line.split(" | ")[0]
+            value = line.split(" | ")[1]
+            value = value.split(", ")
+
+            info = {}
+            # get left and right part for the info of the entry
+            for each in value:
+                x = each.split(": ")[0]
+                y = each.split(": ")[1]
+                info[x] = y
+
+            # update the contacts with the entry
+            contacts[key] = info
+    return contacts
+
+
+if __name__ == "__main__":
+    db = open_database()
+    print(db)
